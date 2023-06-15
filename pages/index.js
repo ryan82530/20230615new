96306d501003b6b181ed7019e4bfd542eb209f36
ryan82530 <134581136+ryan82530@users.noreply.github.com>
2023-06-15 15:57:45 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>I am Ryan, a normal human from the EARTH!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Just work for FREEDOM and Live OURLIFE forever." />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
